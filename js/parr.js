65cb29d6bbe7c4c4ac7cc8fddbb2a6bd080f6e6a
Heather McCullough <heather.mccullough@noaa.gov>
2015-12-24 11:28:45 -0700

    reformatted parr page for ngdc domain

--- js/parr.js
+++ js/parr.js
@@ -4,7 +4,69 @@
 
 
 
-/$(document).ready(function(){
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+var headers = $('#accordion .accordion-header');
+var contentAreas = $('#accordion .ui-accordion-content ').hide();
+var expandLink = $('.accordion-expand-all');
+
+// add the accordion functionality
+headers.click(function() {
+    var panel = $(this).next();
+    var isOpen = panel.is(':visible');
+
+    // open or close as necessary
+    panel[isOpen? 'slideUp': 'slideDown']()
+        // trigger the correct custom event
+        .trigger(isOpen? 'hide': 'show');
+
+    // stop the link from causing a pagescroll
+    return false;
+});
+
+// hook up the expand/collapse all
+expandLink.click(function(){
+    var isAllOpen = $(this).data('isAllOpen');
+
+    contentAreas[isAllOpen? 'hide': 'show']()
+        .trigger(isAllOpen? 'hide': 'show');
+});
+
+// when panels open or close, check to see if they're all open
+contentAreas.on({
+    // whenever we open a panel, check to see if they're all open
+    // if all open, swap the button to collapser
+    show: function(){
+        var isAllOpen = !contentAreas.is(':hidden');
+        if(isAllOpen){
+            expandLink.text('Collapse All')
+                .data('isAllOpen', true);
+        }
+    },
+    // whenever we close a panel, check to see if they're all open
+    // if not all open, swap the button to expander
+    hide: function(){
+        var isAllOpen = !contentAreas.is(':hidden');
+        if(!isAllOpen){
+            expandLink.text('Expand all')
+                .data('isAllOpen', false);
+        }
+    }
+});
+/*
+$('.closeall').click(function(){
+    $('.accordi.in')
+        .collapse('hide');
+});
+$('.openall').click(function(){
+    $('.panel-collapse:not(".in")')
+        .collapse('show');
+});
+*/
+
+/*$(document).ready(function(){
     $("#startButton").click(function(){
         $(this).button('complete');
         $(this).removeClass('btn');
@@ -14,39 +76,4 @@
         $(this).collapse('parr-question-buttons');
     });
 });
-
-/**
- * Toggles between abbreviated and extended views
-
-$(document).ready(function() {
-    if ($("div.span7").height() > 90) {
-        $("div.span7").addClass("viewLess");
-        $("div.span7").html($("div.span7").html() + "<a href=\"javascript:toggleAbstract()\">Show More</a><div class=\"gradient\"></div><div class=\"cover\"></div>");
-    } else {
-        $("div.span7").removeClass("viewLess");
-        $("div.span7").html($("div.span7").html().split("<a href")[0]);
-    }
-});
-
-function toggleAbstract() {
-    if ($("div.span7").hasClass("viewLess")) {
-        $("div.span7").css("height", "auto");
-        var targetHeight = $("div.span7").height();
-        $("div.span7").css("height", "190px");
-        $("div.span7").animate({
-            height: targetHeight
-        }, 300, function() {
-            $("div.span7").removeClass("viewLess");
-            $("div.span7").addClass("viewMore");
-            $("div.span7 a").html("Show Less");
-        });
-    } else {
-        $("div.span7").animate({
-            height: '190px'
-        }, 300, function() {
-            $("div.span7").removeClass("viewMore");
-            $("div.span7").addClass("viewLess");
-            $("div.span7 a").html("Show More");
-        });
-    }
-}*/
+*/
